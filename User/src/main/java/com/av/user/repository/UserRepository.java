04d0db429b491cfdff701e65e3738ddbc050665b
Ali Valizadeh <ali.vlizadeh1@gmail.com>
2023-05-14 21:24:47 +0330

    User Service Upgraded.

--- User/src/main/java/com/av/user/repository/UserRepository.java
+++ User/src/main/java/com/av/user/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.av.user.repository;
 
 import com.av.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
