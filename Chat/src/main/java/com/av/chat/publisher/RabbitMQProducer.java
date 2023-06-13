9cb9fea1bffc3a083d6aa2ed21770455a0dc07fc
Ali Valizadeh <ali.vlizadeh1@gmail.com>
2023-06-13 17:49:32 +0330

    User Service Upgraded.

--- Chat/src/main/java/com/av/chat/publisher/RabbitMQProducer.java
+++
@@ -1,5 +0,0 @@
-package com.av.chat.publisher;
-
-public interface RabbitMQProducer {
-    void sendMessage(Long userIdFrom , Long userIdTo , String messageId);
-}
--- Chat/src/main/java/com/av/chat/request/RequestToRabbitMQ.java
+++
@@ -1,8 +0,0 @@
-package com.av.chat.request;
-
-public record RequestToRabbitMQ(
-        Long userIdFrom ,
-        Long userIdTo ,
-        String messageId
-) {
-}
